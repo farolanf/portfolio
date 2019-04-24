5a47f373d5cbd3f965a119258abfb0b3d81b986a
Farolan Faisal <farolanfaisal@gmail.com>
2019-04-24 08:01:01 +0800

    init config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Farolan Faisal`,
+    description: `Personal website of Farolan Faisal, a friendly full stack web developer.`,
+    author: `Farolan Faisal`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,12 +18,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Farolan Faisal`,
+        short_name: `Farol`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
+        background_color: `#FF0000`,
+        theme_color: `#00FF00`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
